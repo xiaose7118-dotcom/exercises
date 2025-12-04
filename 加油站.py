3becd7f84b524eedd0e9a9c71702313b91f31388
xiaos <xiaose7118@gmail.com>
2025-12-04 20:59:58 +0800

    add new

---
+++ 加油站.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+# @File  : 加油站.py
+# @Author: liaojunfei
+# @Date  : 2025/12/4
+"""
+在一条环路上有 n 个加油站，其中第 i 个加油站有汽油 gas[i] 升。
+
+你有一辆油箱容量无限的的汽车，从第 i 个加油站开往第 i+1 个加油站需要消耗汽油 cost[i] 升。你从其中的一个加油站出发，开始时油箱为空。
+
+给定两个整数数组 gas 和 cost ，如果你可以按顺序绕环路行驶一周，则返回出发时加油站的编号，否则返回 -1 。如果存在解，则 保证 它是 唯一 的。
+
+
+
+示例 1:
+
+输入: gas = [1,2,3,4,5], cost = [3,4,5,1,2]
+输出: 3
+解释:
+从 3 号加油站(索引为 3 处)出发，可获得 4 升汽油。此时油箱有 = 0 + 4 = 4 升汽油
+开往 4 号加油站，此时油箱有 4 - 1 + 5 = 8 升汽油
+开往 0 号加油站，此时油箱有 8 - 2 + 1 = 7 升汽油
+开往 1 号加油站，此时油箱有 7 - 3 + 2 = 6 升汽油
+开往 2 号加油站，此时油箱有 6 - 4 + 3 = 5 升汽油
+开往 3 号加油站，你需要消耗 5 升汽油，正好足够你返回到 3 号加油站。
+因此，3 可为起始索引。
+示例 2:
+
+输入: gas = [2,3,4], cost = [3,4,3]
+输出: -1
+解释:
+你不能从 0 号或 1 号加油站出发，因为没有足够的汽油可以让你行驶到下一个加油站。
+我们从 2 号加油站出发，可以获得 4 升汽油。 此时油箱有 = 0 + 4 = 4 升汽油
+开往 0 号加油站，此时油箱有 4 - 3 + 2 = 3 升汽油
+开往 1 号加油站，此时油箱有 3 - 3 + 3 = 3 升汽油
+你无法返回 2 号加油站，因为返程需要消耗 4 升汽油，但是你的油箱只有 3 升汽油。
+因此，无论怎样，你都不可能绕环路行驶一周。
+"""
+class Node():
+    def __init__(self,gas_value,cost_value,node,ind):
+        self.gas = gas_value
+        self.cost = cost_value
+        self.ind = ind
+        self.next = node
+
+def station(gas,cost):
+    node_list = [Node(g_c[0],g_c[1],None,ind) for ind, g_c in enumerate(zip(gas,cost))]
+    for ind,ndoe in enumerate(node_list):
+        if ind == len(node_list)-1:
+            node_list[ind].next = node_list[0]
+        else:
+            ndoe.next = node_list[ind+1]
+    for ind,node in enumerate(node_list):
+        start = 0
+        while True:
+            g = node.gas
+            c = node.cost
+            start += g
+            start -= c
+            if start < 0:
+                break
+            if node.next.ind == ind:
+                return ind
+            node = node.next
+    return -1
+
+
+
+if __name__ == '__main__':
+    print(station([1,2,3,4,5],[3,4,5,1,2]))
+    print(station([2,3,4],[3,4,3]))
